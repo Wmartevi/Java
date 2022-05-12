da6c847771c9b5925cddcc9461aaae3fb4be9bf6
WELLINGTON MARTEVI DE LIMA <t-wellington.m.lima@bradesco.com.br>
2022-05-12 16:43:30 -0300

    Estruturas condicionais exercicio

--- src/academy/devdojo/maratonajava/basic/EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/basic/EstruturasCondicionais.java
@@ -3,5 +3,27 @@ package academy.devdojo.maratonajava.basic;
 public class EstruturasCondicionais {
     public static void main(String[] args) {
 
+        int idade = 18;
+        boolean isAutorizado = idade >= 18;
+
+        if (isAutorizado){
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }else{
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }
+
+        if (!isAutorizado){
+            System.out.println("Não Autorizado a comprar bebida alcólica");
+        }else {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        }
+
+        //Prova Certificação
+        boolean c = false;
+
+        if (c = true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
     }
 }
