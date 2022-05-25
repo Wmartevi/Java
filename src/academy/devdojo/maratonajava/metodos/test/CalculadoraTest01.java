78ad0c70fc857020a9f28f9c0bc6e0e8bcac0ee2
WELLINGTON MARTEVI DE LIMA <t-wellington.m.lima@bradesco.com.br>
2022-05-25 08:53:12 -0300

    metodos

---
+++ src/academy/devdojo/maratonajava/metodos/test/CalculadoraTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.metodos.test;
+
+import academy.devdojo.maratonajava.metodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+        calculadora.multiplicaDoisNumeros(4, 3);
+
+        double resultado = calculadora.divideDoisNumeros(3,0);
+        System.out.println(resultado);
+    }
+}
